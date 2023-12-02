14b6acdf39fb8f780e5710a32d19430af6008c92
MianIbrahimKhalid <ibrahimkhalid.chs@gmail.com>
2023-12-03 01:38:47 +0500

    done

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+    console.log(`Server listening on http://localhost:${port}`);
+});
